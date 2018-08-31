1526e37dfcf40f69332881fcfa3df51b88ea3e60
Thijs Bouwes <thijsbouwes@gmail.com>
2018-08-31 22:17:31 +0200

    Add games

---
+++ database/migrations/2018_08_31_221310_create_game_user_pivot_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGameUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('game_id');
+
+            $table->timestamps();
+            
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+            $table
+                ->foreign('game_id')
+                ->references('id')
+                ->on('games')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_user_pivot');
+    }
+}
